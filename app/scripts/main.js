cf8d24f3de27e133528dd734fecb48aa17bbabb9
etomarat <etomarat@gmail.com>
2015-03-31 20:51:28 +0300

    final

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -33,4 +33,20 @@ $(function() {
     
   });
   
+  setInterval(function(){
+    $next.click();
+  }, 5000)
+  
+  $(".video-link").fancybox({
+    maxWidth	: 800,
+    maxHeight	: 600,
+    fitToView	: false,
+    width		: '70%',
+    height		: '70%',
+    autoSize	: false,
+    closeClick	: false,
+    openEffect	: 'none',
+    closeEffect	: 'none'
+  });
+  
 });
